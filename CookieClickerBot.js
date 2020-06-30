d85ba23ee5d443361e1b1354057e4c76112b57ad
DevL0rd <dmhzmxn@gmail.com>
2020-06-30 12:34:31 -0600

    Timing system a bit better.

--- CookieClickerBot.js
+++ CookieClickerBot.js
@@ -1,5 +1,4 @@
-var buyAheadBase = 4300;
-
+var buyAheadBase = 120000; //2 Minutes max time, probably gets better to wait longer though, as long as there are buyout rules for ex
 // Pre-req functions
 function multiplyBy(x, y, n) {
     var tot = x * y;
@@ -9,9 +8,8 @@ function multiplyBy(x, y, n) {
     return tot;
 };
 
-var buyAheadMaxTime = 0;
-var clickedCookies = 0;
 var _$ = $;
+var buyAheadMaxTime = 5000;
 var mousClicksPerSecond = 0;
 var cpsTracker = 0;
 var cpsEnd = (new Date()).getTime() + 1000;
@@ -32,31 +30,43 @@ var upgradePaused = false;
 var $targetProduct;
 var targetProductTimeNeeded = 1;
 var $lastSelectedProduct;
-function clickDahUpgrade() {
-    if (upgradePaused) return;
+var buyAllMode = false;
+
+function buyUpgrades() {
     var upgrds = _$("#upgrades").getElementsByClassName("enabled");
     for (upgrd of upgrds) {
         upgrd.click();
-        // _$("#promptOption0").click();
+        // _$("#promptOption0").click(); // check if there first
         console.log("Purchased upgrade.");
-        return
+        return true;
     }
+}
+function buyResearch() {
     var tupgrds = _$("#techUpgrades").getElementsByClassName("enabled");
     for (tupgrd of tupgrds) {
         tupgrd.click();
         console.log("Purchased research.");
-        return
+        return true;
     }
+}
+function clickaShimmer() {
     if (_$(".shimmer")) {
-        if (!/wrath/.test(_$(".shimmer").style.background)) {
-            _$(".shimmer").click();
-            _$("#seasonPopup").click();
-        }
-        console.log("Clicked golden cookie.");
+        // if (!_$(".shimmer").style.backgroundImage.includes("wrath") && !_$(".shimmer").style.backgroundImage.includes("clot")) {
+        _$(".shimmer").click();
+        _$("#seasonPopup").click();
+        // }
+        console.log("Clicked shimmer.");
     }
+}
+function clickDahUpgrade() {
+    if (upgradePaused) return;
+    if (buyUpgrades()) return;
+    if (buyResearch()) return;
+    clickaShimmer();
+
     var lumpsAmount = Number.parseInt(_$("#lumpsAmount").innerHTML)
     if (lumpsAmount) {
-        Game.clickLump();
+        _$("#lumps").click()
         console.log("Obtained lump.");
     }
 
@@ -75,8 +85,9 @@ function clickDahUpgrade() {
 
         var price = Game.Objects[prodName].getPrice();
         var cookiesNeeded = price - cookies;
-        timeNeeded = (cookiesNeeded / cookiesPs) * 1000;
-        if (timeNeeded > 0 && timeNeeded < buyAheadMaxTime) { //It's going to take to long for cookies for the next thing, 
+        timeNeeded = Math.floor(((cookiesNeeded / cookiesPs) * 1000));
+
+        if (!buyAllMode && timeNeeded > 0 && timeNeeded < buyAheadMaxTime) { //It's going to take to long for cookies for the next thing, 
             // console.log("Target:" + timeNeeded + "Ms MouseCPS " + mousClicksPerSecond + "")
             targetProductTimeNeeded = timeNeeded;
             $targetProduct = $prod;
@@ -113,31 +124,33 @@ function clickDahUpgrade() {
     }
 
 }
+var lastBestProd;
 function updateBuyRate() {
     var prods = _$("#products").getElementsByClassName("unlocked");
     var prodName = prods[prods.length - 1].getElementsByClassName("title")[0].innerHTML;
-    if (prodName == "Prism") {
-        buyAheadMaxTime = multiplyBy(buyAheadBase, 1.9, 10);
-    } else if (prodName == "Antimatter condenser") {
-        buyAheadMaxTime = multiplyBy(buyAheadBase, 2, 9);
-    } else if (prodName == "Time machine") {
-        buyAheadMaxTime = multiplyBy(buyAheadBase, 2, 8);
-    } else if (prodName == "Portal") {
-        buyAheadMaxTime = multiplyBy(buyAheadBase, 2, 7);
-    } else if (prodName == "Alchemy lab") {
-        buyAheadMaxTime = multiplyBy(buyAheadBase, 2, 6);
-    } else if (prodName == "Shipment") {
-        buyAheadMaxTime = multiplyBy(buyAheadBase, 2, 5);
-    } else if (prodName == "Wizard tower") {
-        buyAheadMaxTime = multiplyBy(buyAheadBase, 2, 4);
-    } else if (prodName == "Factory") {
-        buyAheadMaxTime = multiplyBy(buyAheadBase, 2, 3);
-    } else if (prodName == "Mine") {
-        buyAheadMaxTime = multiplyBy(buyAheadBase, 2, 2);
-    } else if (prodName == "Farm") {
-        buyAheadMaxTime = multiplyBy(buyAheadBase, 2, 1);
-    } else if (prodName == "Grandma") {
+    if (prodName.includes("<span")) {
+        prodName = prodName.split(">")[1].split("<")[0];
+    }
+    if (["Alchemy lab", "Portal", "Time machine", "Chancemaker", "Prism", "Chancemaker", "Fractal engine", "Javascript console"].includes(prodName)) {
         buyAheadMaxTime = buyAheadBase;
+    } else if (["Wizard tower", "Shipment"].includes(prodName)) {
+        buyAheadMaxTime = buyAheadBase / 1
+    } else if (prodName == "Temple") {
+        buyAheadMaxTime = buyAheadBase / 2
+    } else if (prodName == "Bank") {
+        buyAheadMaxTime = buyAheadBase / 3
+    } else if (prodName == "Factory") {
+        buyAheadMaxTime = buyAheadBase / 4
+    } else if (prodName == "Mine") {
+        buyAheadMaxTime = buyAheadBase / 5
+    } else if (prodName == "Farm") {
+        buyAheadMaxTime = buyAheadBase / 6
+    } else if (prodName == "Grandma") {
+        buyAheadMaxTime = buyAheadBase / 7;
+    }
+    if (lastBestProd != prodName) {
+        console.log("Unlocked product '" + prodName + "'. Max wait time '" + (buyAheadMaxTime / 1000) + " Ms'")
+        lastBestProd = prodName;
     }
 }
 
@@ -168,5 +181,12 @@ function pauseUpgrades() {
     }
     return upgradePaused;
 }
-
+function buyAll() {
+    if (buyAllMode) {
+        buyAllMode = false;
+    } else {
+        buyAllMode = true;
+    }
+    return buyAllMode;
+}
 
